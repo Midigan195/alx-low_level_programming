960cb4cad489210b19901e5a8f5bfa939d60a777
Liam Bowers <liambowers718@gmail.com>
2023-02-16 05:06:49 -0800

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * text - declare array
+ *
+ * puts - prints text to output
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	
+{
 	char text[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);	
 	return (0);
 }

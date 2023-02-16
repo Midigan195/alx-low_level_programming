f74b282ac93be8e6b7afcaacb710fd8d0cb11160
Liam Bowers <liambowers718@gmail.com>
2023-02-16 05:36:28 -0800

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * text - declare array
+ *
+ * puts - prints text to output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", text);
+	return (0);
+}

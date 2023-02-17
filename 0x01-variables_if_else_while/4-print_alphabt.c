24d0d4a68e8d2c8290597f0cc689617df577df6c
Liam Bowers <liambowers718@gmail.com>
2023-02-17 03:02:49 -0800

    Letters of aplabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+*
+*
+*
+*
+*/
+int main(void)
+{
+	char i = 'a';
+
+	while (i < 'z')
+	{
+		if (!(i == 'q' || i == 'e'))
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,4 @@
 0 - Determines wether number is positive or negative
 1- Gets last number and prints >5,0 or <6not 0
+2- Print alphabet
+3- print lowercase and uppercase alpahbet

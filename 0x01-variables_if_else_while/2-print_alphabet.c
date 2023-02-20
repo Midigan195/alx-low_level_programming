846044a1e15382de08127524edc42d5df35da6c6
Liam Bowers <liambowers718@gmail.com>
2023-02-20 02:36:56 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
 /**
-*
-*
-*
-*
+ * main - runs program
+ *
+ * Return: 0 if sucessful
 */
 int main(void)
 {
 	char i = 'a';
 
-	while (i < 'z')
+	while (i < ('z'+1))
 	{
 		putchar(i);
 		i++;

367c53b7116f33cff67aca03f930749a59ce6a73
Liam Bowers <liambowers718@gmail.com>
2023-02-20 03:13:14 -0800

    array

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 
 /**
-*
-*
-*
-*
+ * main - runs program
+ *
+ * Return: 0 if sucessful
 */
 int main(void)
 {
 	int i = 48;
-	
+
 	while (i < 58)
 	{
 		putchar(i);
 		if (i < 57)
 		{
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 		i++;
 	}

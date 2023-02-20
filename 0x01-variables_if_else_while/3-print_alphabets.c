14894816534a1f7a9e268fcef348dae422e630d8
Liam Bowers <liambowers718@gmail.com>
2023-02-20 02:44:50 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 
 /**
-*
-*
-*
-*
+ * main - run program
+ *
+ * i: initialise a
+ * j: initialise A
+ *
+ * Return: 0 if sucesfful
 */
 int main(void)
 {
 	char i = 'a';
 	char j = 'A';
 
-	while (i < 'z')
+	while (i < ('z' + 1))
 	{
 		putchar(i);
 		i++;
 	}
 
-	while (j < 'Z')
+	while (j < ('Z' + 1))
 	{
 		putchar(j);
 		j++;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }

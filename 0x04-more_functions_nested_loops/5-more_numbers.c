d965b5ea4f7fc4a7c16ea5f10cd3ba6ed1e5f9d5
Liam Bowers <liambowers718@gmail.com>
2023-02-23 02:33:40 -0800

    displays numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * more_numbers - Checks if letter is uppercase
+ * Return: 0 always sucessful
+*/
+
+void more_numbers(void)
+{
+	char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int i = 0;
+	int j = 0;
+
+	while (j < 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i < 10)
+			{
+				_putchar(c[i]);
+			}
+			else
+			{
+				_putchar('1');
+				_putchar(c[i % 10]);
+			}
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}

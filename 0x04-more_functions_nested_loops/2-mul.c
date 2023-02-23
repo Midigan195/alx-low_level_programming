3bf215652d5c68dadb146d46fc04d9e80dac43f5
Liam Bowers <liambowers718@gmail.com>
2023-02-23 01:55:58 -0800

    multiply digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * mult - Checks if letter is uppercase
+ * @a: first value
+ * @b: second value
+ * Return: multiplication value
+*/
+
+int mul(int a, int b)
+{
+	return (a*b);
+}

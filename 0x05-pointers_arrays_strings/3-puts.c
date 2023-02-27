2cbd33900ee6be3d9e7df8138ae7841274babc1e
Liam Bowers <liambowers718@gmail.com>
2023-02-27 05:16:36 -0800

    print string with pointer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,14 +9,18 @@ void _puts(char *str)
 {
 	int i = 0;
 	char c;
+	char c2;
 
 	while (c != '\0')
 	{
-		if (c != '\0')
+		if (c2 != '\0')
 		{
+			c2 = *(str + (i + 1));
 			c = *(str + i);
 			_putchar(c);
 		}
+		else
+			break;
 		i++;
 	}
 	_putchar('\n');

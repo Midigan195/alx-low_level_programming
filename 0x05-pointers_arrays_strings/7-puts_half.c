8c97c86791d2777c688c5921435e14648bc1e07a
Liam Bowers <liambowers718@gmail.com>
2023-02-27 08:44:15 -0800

    print half the values

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * puts_half - prints seconfd half of string
+ * @str: takes pointer of character
+ * Return: nothing
+*/
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	len--;
+
+	if (len % 2)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	n++;
+	while (n < len)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
+

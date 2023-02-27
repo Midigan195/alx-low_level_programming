86accd6e410cfa9b7db5ff4a433118b8b6cd921a
Liam Bowers <liambowers718@gmail.com>
2023-02-27 06:35:32 -0800

    rverese

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _print_rev - prints string
+ * @s: takes pointer of character
+ * Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	while (!(i < 0))
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}

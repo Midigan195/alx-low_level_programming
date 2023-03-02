b112cfceab452ad88e296c40167ac02b40c1b56b
Liam Bowers <liambowers718@gmail.com>
2023-03-02 05:39:58 -0800

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strcmp - concattenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: value of dest
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int k = 0;
+	int val = 0;
+
+	while (*(s1 + k) != '\0' && *(s2 + k) != '\0')
+	{
+		val = *(s1 + k) - *(s2 + k);
+
+		if (val != 0)
+			break;
+		else 
+			k++;
+	}
+		val = *(s1 + k) - *(s2 + k);
+
+	return (val);
+}

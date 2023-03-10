354ecb74eadf7cb11f164822e68ef781e95df795
Liam Bowers <liambowers718@gmail.com>
2023-03-10 02:41:37 -0800

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+/**
+ * main - returns name of executable
+ * @argv: array of srguments
+ * Return: (0) always success
+*/
+
+int main (int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		while (i < (argc))
+		{
+			if (!((*argv[i] >= '0') && (*argv[i] <= '9')))
+			{
+				printf("Error\n");
+				return (1);
+			}	
+			sum = sum + atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}

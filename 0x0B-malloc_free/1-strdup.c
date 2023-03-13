57f90c9053a192e1a450f406892942b48d1d432a
Liam Bowers <liambowers718@gmail.com>
2023-03-13 02:51:22 -0700

    copy array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * _strdup - Creates array and fills with specific char
+ * @str: character
+ * Return: pointer to array
+*/
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i = 0;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\n')
+		i++;
+	i++;
+
+	arr = malloc(sizeof(char) * i );
+	if (arr == NULL)
+		return (NULL);
+	
+	while (j < i)
+	{
+		arr[j] = *(str + j);
+		j++;
+	}
+
+	return (arr);
+
+}

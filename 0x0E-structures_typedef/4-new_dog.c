c3c74e73bc38c71aa35146b18b8b81a9be70f6b4
Liam Bowers <liambowers718@gmail.com>
2023-03-20 11:53:50 -0700

    Add new member to dog structure

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include"dog.h"
+/**
+ * new_dog - pointer to a new member
+ * @name: name of dog
+ * @age: age of dog
+ * @owner : owner of dog
+ * return: pointer to new member
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	
+	return (dog);
+}

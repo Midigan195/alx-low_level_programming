a3fd2f125f7ff7afe4ec7521eb6627e049f5f326
Liam Bowers <liambowers718@gmail.com>
2023-03-20 08:24:51 -0700

    Create struct in header file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - contains details of each dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: name of dogs owner
+ * Description: holds the name ange and name of dog's owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* define dog_h */

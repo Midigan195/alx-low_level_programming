7d865e91cb957035c6a0305514f757e5317bd302
Liam Bowers <liambowers718@gmail.com>
2023-03-22 03:28:17 -0700

    Apply function to each element in array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include"function_pointers.h"
+#include<stddef.h>
+/**
+ * array_iteration - iterates action through every element in array
+ * array - pointer to array
+ * size - size of array
+ * action - pointer to function
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

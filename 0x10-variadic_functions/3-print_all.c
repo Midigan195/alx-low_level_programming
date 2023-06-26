f5a0dfb695b701f17fe870ea99998eb1f42cbb74
Liam Bowers <liambowers718@gmail.com>
2023-06-26 10:29:59 -0700

    Print any variable of any data type to terminal

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include<stddef.h>
+/**
+ * print_all - prints any number of arguments of and data type
+ * @format: pointer to a list of data types of each argument
+*/
+void print_all(const char *format, ...)
+{
+	unsigned int j = 0;
+	char *string = NULL;
+	va_list ptr;
+
+	va_start(ptr, format);
+
+	while (format[j] != '\0')
+	{
+		switch (format[j])
+		{
+			case 'c':
+				printf("%c", (va_arg(ptr, int)));
+				break;
+			case 'i':
+				printf("%i", (va_arg(ptr, int)));
+				break;
+			case 'f':
+				printf("%f", (va_arg(ptr, double)));
+				break;
+			case 's':
+				string = va_arg(ptr, char*);
+				if (string == NULL)
+					printf("(nil)");
+				else
+					printf("%s", string);
+				break;
+			default:
+				break;
+		}
+		if ((format[j] == 'c' || format[j] == 'i' || format[j] == 'f' || format[j] == 's') && (format[j + 1] != '\0'))
+			printf(",");
+		j++;
+	}
+	printf("\n");
+
+	va_end(ptr);
+}

9e6b8be6ece1d039c28bccab5adf23240ef2a0be
Midigan195 <100342590+Midigan195@users.noreply.github.com>
2023-06-26 06:54:06 +0200

    2-calloc.c Buffer added

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,22 +9,25 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	void *p = NULL;
+	char *buff = NULL;
 	int i = 0;
+	int space = nmemb * size;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	buff = malloc(space);
+	if (buff == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
 	
-	while (i < (nmemb * size))
+	while (i < (space))
 	{
-		*(char*)(p + i) = 0;
+		*(buff + i) = 0;
 		i++;
 	}
+	p = buff;
 	return (p);
 }

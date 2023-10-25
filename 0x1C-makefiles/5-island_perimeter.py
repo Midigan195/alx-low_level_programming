6391e10a181c7ac7d31214aae09f617a04334792
Liam Bowers <liambowers718@gmail.com>
2023-10-25 06:09:22 -0700

    Create a makefile with an object clean

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,23 +9,18 @@ def island_perimeter(grid):
     This function returns the total perimeter of an island on a grid
     """
     perimeter = 0
+    col_len = len(grid)
+    row_len = len(grid[0])
 
-    if len(grid) == 0 or len(grid[0]) == 0 or grid:
-        return perimeter
-    else:
-        col_len = len(grid)
-        row_len = len(grid[0])
-
-    for col in range(len(grid)):
-        for row in range(len(grid[col])):
-
+    for col in range(col_len):
+        for row in range(row_len):
             if grid[col][row] == 1:
-                if row > row_len or grid[col][row+1] == 0:
+                if row >= row_len-1 or grid[col][row+1] == 0:
                     perimeter += 1
                 if row < 0 or grid[col][row-1] == 0:
                     perimeter += 1
                 if col < 0 or grid[col-1][row] == 0:
                     perimeter += 1
-                if col > row_len or grid[col+1][row] == 0:
+                if col >= col_len-1 or grid[col+1][row] == 0:
                     perimeter += 1
     return perimeter
